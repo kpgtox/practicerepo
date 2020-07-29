555640e7adadea9f0a5364bc2e741d6a84a8f0dd
karthikp <kpmac13@Vishistas-MacBook-Pro.local>
2020-07-29 09:30:55 +0530

    p

--- simplilearn/js_practice.js
+++ simplilearn/js_practice.js
@@ -1,2 +1,29 @@
-    console.log("can be written js in both head and body sections.");
-    console.log("can be written js in separate js files linked to the html file.");
+console.log("can be written js in both head and body sections.");
+console.log("can be written js in separate js files linked to the html file.");
+
+// numeric addition
+var a = 10;
+var b = 23342.12;
+console.log(a+b);
+
+// string addition or concatenation
+var foo = "hello";
+var boo = "human";
+console.log(foo + " " + boo);
+
+// Get data type of a variable
+console.log(typeof boo)
+console.log(typeof a);
+
+// declaring but not initializing(defining) variable
+var moo;
+console.log(typeof moo); //results undefined.
+
+//null object
+var c = null;
+console.log(typeof c);
+
+// check if null is undefined
+console.log( null == undefined )
+// check if null is of the type undefined
+console.log( null === undefined )
