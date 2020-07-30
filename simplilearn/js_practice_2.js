a6d683f43f5c4199d300990789ff8af756471011
karthikp <kpmac13@Vishistas-MacBook-Pro.local>
2020-07-30 05:58:51 +0530

    p

--- simplilearn/js_practice_2.js
+++ simplilearn/js_practice_2.js
@@ -28,3 +28,28 @@ console.log(typeof person)
 //retreive a value from a js object
 console.log(person.firstname);
 console.log(person.age);
+
+//write something on the webpage
+document.write("hello there " + person.firstname);
+
+//declaring and defining/initializing a js object with some key valuepairs.
+var bosses = { turtles:"shredder",
+               residentevil:"wesker",
+               yakuza:"ryuji goda",
+               dmc:"vergil"};
+
+console.log("accessing and printing bosses object or data structure")
+console.log(bosses);
+console.log(bosses.dmc);
+console.log(bosses["yakuza"]);
+
+var girls = {name1:"chrissy",
+             name2:"julia",
+             name3:"elizabeth"};
+
+//concatanating strings with an object's key
+//dynamic and flexible code format of a ds object to loop through or access an object.
+var i=1;
+console.log(girls["name"+i]);
+i++;
+console.log(girls["name"+i]);
