cd7a3c34d4503d6da96ce78c1f7679b3403b0543
“kpgtox” <nexuscodezone@gmail.com>
2020-10-21 00:02:51 +0200

    p

---
+++ Eduonix-WebDevComplete15Projects/FruitSlice_Game/game_jq.js
@@ -0,0 +1,5 @@
+// click on start/reset
+// if playing, reload else : show lives, change button text, create fruit, move fruit by one step, check fruit level,
+
+// print something first to check if this js file is linked to the html page:
+console.log("hellllo");
