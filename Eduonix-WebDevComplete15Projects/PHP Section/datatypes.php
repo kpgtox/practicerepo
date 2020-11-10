7e259c5a1a896455743c57ad100f6a2132242e57
“kpgtox” <nexuscodezone@gmail.com>
2020-11-10 13:19:14 +0100

    p

--- Eduonix-WebDevComplete15Projects/PHP Section/datatypes.php
+++ Eduonix-WebDevComplete15Projects/PHP Section/datatypes.php
@@ -17,6 +17,55 @@ $age = 21;
 echo "Axel's age is $age";
 
 //Integers:
+$x  = 10;
+
+// Get datatype and the value of a variable:
+var_dump($x);
+
+// Operations on integers:
+$y = 11;
+$z = $x + $y;
+echo '<br/>';
+echo $z;
+
+//hexa decimal integers:
+$hex = 0x1A;
+//move cursor to new line:
+echo '<br/>';
+//prints value of the variable hex in decimal base (base 10):
+var_dump($hex);
+
+//Octa decimal integers:
+$octa = 0123;
+//move cursor to new line:
+echo '<br/>';
+//prints value of the variable octa in decimal base (base 10):
+var_dump($octa);
+
+// floats:
+$f = 3.5;
+echo '<br/>';
+var_dump($f);
+
+// Test a condition:
+$boo = (5<6);
+// prints datatype and value of boo:
+var_dump($boo);
+
+// Testing AND, OR condition:
+$boo2 = (6>7);
+$boo3 = $boo && $boo2;
+var_dump($boo3);
+$boo3 = $boo || $boo2;
+var_dump($boo3);
+
+//Not condiiton:
+var_dump(!$boo3);
+
+
+
+
+
 
 
 
