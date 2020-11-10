6dfc77b469d43ed1ce4ebff51c1441ca2e2e37e3
“kpgtox” <nexuscodezone@gmail.com>
2020-11-10 13:01:42 +0100

    p

---
+++ Eduonix-WebDevComplete15Projects/PHP Section/datatypes.php
@@ -0,0 +1,24 @@
+<?php
+
+//Declare and print a string with a line break html tag:
+$name = 'Axel';
+//concatenate and print:
+echo 'Name: ' . $name . '<br/>';
+
+//Using single quotes in a string:
+$message = "Axel's finishing move."
+echo $message . '<br/>';
+
+//Concatenating without the .
+//only use double quotes when using the variable's name directlyto contcatenate.
+echo "Best Fighter is $name";
+$age = 21;
+// age is treated as a string automatically:
+echo "Axel's age is $age";
+
+//Integers:
+
+
+
+
+?>
---
+++ Eduonix-WebDevComplete15Projects/PHP Section/variables.php
@@ -0,0 +1,18 @@
+<?php
+
+//Declaring Variables and concatenating strings:
+$name = "karthik";
+$age = 31;
+$message = "hi, my name is " . $name . " and i am " . $age . " years old.";
+//print:
+echo $message;
+
+// Declaring constant variables:
+define("COUNTRY", "INDIA");
+define("PRICE", 999);
+//print:
+echo "Flight Destination: " . COUNTRY;
+echo "Price: " . PRICE;
+
+
+?>
