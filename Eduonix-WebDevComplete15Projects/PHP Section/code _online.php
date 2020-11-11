e219d87407f969a588316ccd81c8b15e9035163b
“kpgtox” <nexuscodezone@gmail.com>
2020-11-11 14:35:28 +0100

    p

---
+++ Eduonix-WebDevComplete15Projects/PHP Section/code _online.php
@@ -0,0 +1,15 @@
+<?php
+
+//--
+$array = array("1" => "PHP code tester Sandbox Online",
+              "foo" => "bar", 5 , 5 => 89009,
+              "case" => "Random Stuff: " . rand(100,999),
+              "PHP Version" => phpversion()
+              );
+
+foreach( $array as $key => $value ){
+    echo $key."\t=>\t".$value."\n";
+
+//--
+
+?>
---
+++ Eduonix-WebDevComplete15Projects/PHP Section/conditions.php
@@ -0,0 +1,11 @@
+<?php
+
+$b1 = true;
+if(b1){
+  echo "its true";
+} else {
+  echo "its false";
+}
+
+
+?>
