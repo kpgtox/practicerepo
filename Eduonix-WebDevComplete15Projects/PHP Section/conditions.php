e779a39d39702466db61b68e2be7d840473beda6
“kpgtox” <nexuscodezone@gmail.com>
2020-11-13 18:02:56 +0100

    p

--- Eduonix-WebDevComplete15Projects/PHP Section/conditions.php
+++ Eduonix-WebDevComplete15Projects/PHP Section/conditions.php
@@ -11,4 +11,29 @@ if(b1){
 //if  b1 is true then assign string A to var b2 or else string B.
 $b2 = ($b1)?"A":"B";
 
+$temp = 25;
+if($temp<10){
+  echo "cold";
+}elseif ($temp>35) {
+  echo "hot";
+}else{
+  echo "normal";
+}
+
+// Switch in php:
+
+// check for string match:
+$strength = "hope";
+switch($strength){
+  case("hope"):
+    echo "yess";
+    break;
+  case("life"):
+    echo "keep going";
+    break;
+  default:
+    echo "try harder";
+    break;
+}
+
 ?>
