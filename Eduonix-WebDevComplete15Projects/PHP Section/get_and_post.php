280ac1a7dca27ced399487930d5b227abcba4393
“kpgtox” <nexuscodezone@gmail.com>
2020-11-14 14:18:15 +0100

    p

--- Eduonix-WebDevComplete15Projects/PHP Section/get_and_post.php
+++ Eduonix-WebDevComplete15Projects/PHP Section/get_and_post.php
@@ -1,6 +1,27 @@
 <?php
 
 //Send information to a web server using php:
+//GET method:
+// this php file is mentioned in the action acttribute of a form tag, in an html page.
+echo "<h3>GET:</h3>"
+//prints the information sent using the get method:
+print_r($_GET);
+if($_GET["submit"]){
+  echo "<p>the get form has been submitted.</p>";
+}
+if($_GET["username"]){
+  echo "<p>Hi, " . $_GET["username"] . ". Welcome to my page.</p>";
+}
+
+echo "<h3>POST:</h3>"
+//prints the information sent using the post method:
+print_r($_POST);
+if($_POST["submit"]){
+  echo "<p>the post form has been submitted.</p>";
+}
+if($_POST["country"]){
+  echo "<p>Your country is " . $_POST["country"] . "</p>";
+}
 
 
 
