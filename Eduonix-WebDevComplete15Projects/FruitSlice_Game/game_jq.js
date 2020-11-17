7a889420647f67ed48ef4134c302962ce75a19a9
“kpgtox” <nexuscodezone@gmail.com>
2020-11-17 16:22:18 +0100

    p

--- Eduonix-WebDevComplete15Projects/FruitSlice_Game/game_jq.js
+++ Eduonix-WebDevComplete15Projects/FruitSlice_Game/game_jq.js
@@ -3,7 +3,7 @@
 // if playing, reload else : show lives, change button text, create fruit, move fruit by one step, check fruit level,
 
 // print something first to check if this js file is linked to the html page:
-console.log("hellllo");
+// console.log("hellllo");
 
 // All jquery code must go in the following function:
 // $(function(){});
@@ -48,8 +48,15 @@ var lives = 3;
 var fruits = ['apple','banana','grapes','watermelon','orange','pineapple'];
 var fruit_container_width = 550;
 var animate;
+var step = 0;
 
 $(function(){
+  Loadup();
+  Start_game();
+});
+
+function Start_game(){
+  // if start button clicked:
   $("#start_button").click(function(){
     // while playing:
     if(playing){
@@ -58,27 +65,35 @@ $(function(){
     }
     // when game over / page reload:
     else{
-      playing = true;
-      score = 0;
-      $("#score_val").html(score);
-      $("#lives").show();
-      lives = 3;
-      Add_Lives();
-      $("#start_button").html("Reset Game");
-      Start_Action();
+      Reset_game();
     }
   });
-});
+}
 
-function Add_Lives(){
+function Reset_game(){
+  playing = true;
+  score = 0;
+  lives = 3;
+  step = 0;
+  Show_Lives();
+  $("#score_val").html(score);
+  $("#start_button").html("Reset Game");
+  Start_Action();
+}
+
+function Loadup(){
+  $("#start_button").html("Start Game");
+}
+
+function Show_Lives(){
   for(i=0; i<lives; i++){
     $("#lives").append(' <img src="images/heart.png" class="hearts">');
   }
+  $("#lives").show();
 }
 
 function Start_Action(){
   Choose_Fruit();
-  $("#fruit1").show();
   Position_fruit();
   Move_fruit();
 }
@@ -86,6 +101,7 @@ function Start_Action(){
 function Choose_Fruit(){
   var x = Math.round(5*Math.random());
   $("#fruit1").attr('src','images/' + fruits[x] + '.png');
+  $("#fruit1").show();
 }
 
 function Position_fruit(){
@@ -94,13 +110,43 @@ function Position_fruit(){
 }
 
 function Move_fruit(){
+  Generate_step();
+  Add_step();
+}
+
+function Generate_step(){
   //gen a random step to move the fruit with:
-  var step = 1 + Math.round(5*Math.random());
+  step = 1 + Math.round(5*Math.random());
+}
+
+function Add_step(){
+  //Set Interval is a looper function(like a unity coroutine) that only stops when told to:
   //Move fruit down by one random step every 10ms:
   animate = setInterval(function(){
     // edit the top attribute of fruit1's css, add step to its current top position's value:
     $("#fruit1").css('top', $("#fruit1").position().top + step);
+    Check_fruit_pos();
   }, 10);
+}
 
+function Check_fruit_pos(){
+  var x = $("#fruit1").position().top;
+  var y = $("#main").height();
+  if(x>y){
+    Update_lives();
+    // Choose_Fruit();
+    // Position_fruit();
+    // Generate_step();
+  }
+}
 
+function Update_lives(){
+  if(lives > 1){
+    lives--;
+    Show_Lives();
+  }
+  // else{
+  //   lives = 0;
+  //   Show_Lives();
+  // }
 }
